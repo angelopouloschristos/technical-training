ffbcbd91e3c97748dba3a89fa44a197e5dc8adbf
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-12-22 23:19:04 +0100

    .
    .

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -64,10 +64,22 @@ class SaleOrder(models.Model):
         #return user_level >= required_level
         return maxamountapproval
 
-
     def action_request_approval(self):
-        # Envoi d'un message à l'utilisateur pour lui demander d'obtenir l'approbation
-        self.message_post(body="La commande nécessite une approbation de la part d'un gestionnaire de niveau supérieur.")
+        # Sélection du gestionnaire avec le moins d'approbations en attente d'assignation (optionnel)
+        manager = self.select_manager()
+        if not manager:
+            return
+
+        # Création d'une activité pour le gestionnaire sélectionné
+        self.activity_schedule(
+            'mail.activity_data_todo',
+            user_id=manager.id,
+            note="Demande d'approbation de la commande %s" % self.name,
+        )
+
+        # Envoi d'un message à l'utilisateur pour lui signaler que la demande d'approbation a été envoyée au gestionnaire
+        self.message_post(
+            body="La demande d'approbation de la commande a été envoyée au gestionnaire %s" % manager.name)
 
         # Renvoi d'une action pour afficher la commande et lui demander l'approbation
         return {
@@ -77,4 +89,37 @@ class SaleOrder(models.Model):
             'view_mode': 'form',
             'target': 'current',
         }
+
+    def select_manager(self):
+        """
+        Sélectionne le gestionnaire avec le moins d'approbations en attente d'assignation.
+        """
+        # Récupération de tous les gestionnaires de l'entreprise
+        managers = self.env['res.users'].search([('groups_id', 'in', self.env.ref('base.group_user').id)])
+        if not managers:
+            return
+
+        # Initialisation du gestionnaire sélectionné à la première valeur de la liste de gestionnaires
+        selected_manager = managers[0]
+
+        # Recherche du gestionnaire avec le moins d'approbations en attente d'assignation
+        for manager in managers:
+            # Récupération des activités en attente d'assignation du gestionnaire
+            pending_approvals = self.env['mail.activity'].search_count([
+                ('user_id', '=', manager.id),
+                ('state', '=', 'to_do'),
+            ])
+            # Comparaison du nombre d'approbations en attente du gestionnaire courant au nombre d'approbations en attente du gestionnaire sélectionné
+            # Récupération du nombre d'approbations en attente du gestionnaire sélectionné
+            selected_manager_pending_approvals = self.env['mail.activity'].search_count([
+                ('user_id', '=', selected_manager.id),
+                ('state', '=', 'to_do'),
+            ])
+            # Si le nombre d'approbations en attente du gestionnaire courant est inférieur au nombre d'approbations en attente du gestionnaire sélectionné, le gestionnaire courant devient le gestionnaire sélectionné
+            if pending_approvals < selected_manager_pending_approvals:
+                selected_manager = manager
+
+        # Renvoi du gestionnaire sélectionné
+        return selected_manager
+
 
