c580d08b2123290cc34852ecc25efbb5d061df35
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-12-22 22:30:20 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -32,7 +32,7 @@ class SaleOrder(models.Model):
 
                 return super(SaleOrder, self).action_confirm()
         else:
-            raise Exception("Le montant total de la commande dépasse le montant maximal de validation du partenaire.")
+            return self.message_post("Le montant total de la commande dépasse le montant maximal de validation du partenaire.")
 
 
 
