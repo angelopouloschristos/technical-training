81dcc519d7ed47010be26761ba2211ee18fecb66
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-12-25 00:44:29 +0100

    STP
    STP

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,11 +7,12 @@
     'category': 'Uncategorized',
     'license': 'LGPL-3',
     'version': '16.0.0.1',
-    'depends': ['base', 'sale_management', 'calendar', 'hr'],
+    'depends': ['base', 'sale_management', 'calendar', 'hr', ],
     'data': [
-        'views/res_partner.xml',
-        'views/res_groups.xml',
-        'views/sale_order.xml',
-        'views/choose_training_date_wizard_view.xml',
+        "views/res_partner.xml",
+        "views/res_groups.xml",
+        "views/sale_order.xml",
+        "views/choose_training_date_wizard_view.xml",  # Ajout de la vue ici
     ]
+
 }
