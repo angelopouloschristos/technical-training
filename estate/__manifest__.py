8587b8f37668db23e0e94bab2d400bdc24c1df5c
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-12-24 23:20:11 +0100

    CA A TOUT DELETE ...ZEOR¨PGHNEZRUHGEZRJHIZ
    CA A TOUT DELETE ...ZEOR¨PGHNEZRUHGEZRJHIZ

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,11 +8,10 @@
     'license': 'LGPL-3',
     'version': '16.0.0.1',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
-    'views': [
+    'data': [
         'views/res_partner.xml',
         'views/res_groups.xml',
         'views/sale_order.xml',
         'views/choose_training_date_wizard_form.xml',
-    ],
-    'data': []
+    ]
 }
