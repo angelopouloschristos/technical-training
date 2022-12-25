df8e077e30fb6b52f9792f31d47359ce08fb353b
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-12-25 16:09:26 +0100

    .
    .

--- estate/models/choose_training_date_wizard.py
+++ estate/models/choose_training_date_wizard.py
@@ -6,7 +6,6 @@ class ChooseTrainingDateWizard(models.TransientModel):
 
     training_date = fields.Date(string='Training Date', required=True)
 
-    @api.model
     def confirm(self):
         # Récupération de la ligne de commande sélectionnée
         active_id = self._context.get('active_id')
